c73442c14dc3597b91c14883d4e22fa68eb674cd
admin <admin@admins-MacBook-Pro.local>
2024-03-20 16:50:34 +0700

    canvas

--- src/components/Form/image.tsx
+++ src/components/Form/image.tsx
@@ -8,30 +8,36 @@ export default function ImageCanvas(props: {
     width: number;
     height: number;
   };
+  id_current: React.MutableRefObject<any>;
+  newFabricCanvas: any;
 }) {
-  const id_current = React.useRef(null);
   React.useEffect(() => {
-    if (id_current.current === null) return;
+    if (props.id_current.current === null) return;
     if (props.template.width === undefined || props.template.height === undefined) return;
     if (props.template.imageUrl === undefined) return;
-    const canvas = new fabric.Canvas(id_current.current);
+
     fabric.Image.fromURL(`/${props.template.imageUrl}`, function (oImg) {
       const { width, height } = oImg.getOriginalSize();
+      oImg.selectable = false;
       oImg.scaleX = (props.template.width * 3) / width;
       oImg.scaleY = (props.template.height * 3) / height;
-      canvas.add(oImg);
+      props.newFabricCanvas.add(oImg);
     });
-    canvas.renderAll();
+
+    props.newFabricCanvas.renderAll();
     return () => {
-      if (canvas) canvas.dispose();
+      if (props.newFabricCanvas) props.newFabricCanvas.dispose();
     };
-  }, [id_current.current, props.template.width, props.template.height]);
+  }, [props.id_current.current, props.template.width, props.template.height]);
+
   return (
-    <canvas
-      ref={id_current}
-      width={props.template.width * 3}
-      height={props.template.height * 3}
-      style={{ border: "1px solid black" }}
-    ></canvas>
+    <div>
+      <canvas
+        ref={props.id_current}
+        width={props.template.width * 3}
+        height={props.template.height * 3}
+        style={{ border: "1px solid black" }}
+      ></canvas>
+    </div>
   );
 }
--- src/components/Template/editTemplate.tsx
+++ src/components/Template/editTemplate.tsx
@@ -5,8 +5,9 @@ import Button from "@mui/material/Button";
 import ImageIcon from "@mui/icons-material/Image";
 import FilterIcon from "@mui/icons-material/Filter";
 import { styled } from "@mui/material/styles";
-import React from "react";
+import React, { useEffect } from "react";
 import ImageCanvas from "../Form/image";
+import { fabric } from "fabric";
 
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -22,6 +23,12 @@ const VisuallyHiddenInput = styled("input")({
 
 export default function EditTemplate(props: { id: string }) {
   const [template, setTemplate] = React.useState({});
+  const id_current = React.useRef(null);
+  const [newFabricCanvas, setNewFabricCanvas] = React.useState(null);
+  useEffect(() => {
+    const canvas = new fabric.Canvas(id_current.current);
+    setNewFabricCanvas(canvas);
+  }, [id_current.current]);
 
   React.useEffect(() => {
     fetch(`http://192.168.1.222:3000/template/${props.id}`, {
@@ -33,7 +40,16 @@ export default function EditTemplate(props: { id: string }) {
       });
   }, [props.id]);
 
-  console.log(template);
+  const draw = () => {
+    const rect = new fabric.Rect({
+      left: 100,
+      top: 100,
+      fill: "red",
+      width: 20,
+      height: 20,
+    });
+    newFabricCanvas.add(rect);
+  };
 
   return (
     <div>
@@ -53,7 +69,7 @@ export default function EditTemplate(props: { id: string }) {
             {/* <div className='img'>
                             <img src={`/${template.imageUrl}`} alt='Image template' width={template.width} height={template.height}></img>
                         </div> */}
-            <ImageCanvas template={template}></ImageCanvas>
+            <ImageCanvas template={template} id_current={id_current} newFabricCanvas={newFabricCanvas}></ImageCanvas>
           </div>
           <div className="sidebar-content position-relative">
             <div className="flex">
@@ -70,10 +86,12 @@ export default function EditTemplate(props: { id: string }) {
               <div className="px-3 py-3">
                 <div className="border w-20 h-20 rounded-lg flex justify-center items-center hover:bg-layer cursor-pointer">
                   <div className="text-center text-sm">
-                    <div>
-                      <InsertPhotoIcon></InsertPhotoIcon>
-                    </div>
-                    Image
+                    <Button onClick={draw}>
+                      <div>
+                        <InsertPhotoIcon></InsertPhotoIcon>
+                      </div>
+                      Image
+                    </Button>
                   </div>
                 </div>
               </div>
