fa8163fad98c70c914d223448ce9140aff5dba8e
admin <admin@admins-MacBook-Pro.local>
2024-03-20 15:10:39 +0700

    update cloud storage

--- src/components/Form/image.tsx
+++ src/components/Form/image.tsx
@@ -1,23 +1,37 @@
 "use client";
-import { fabric } from 'fabric';
-import React from 'react';
+import { fabric } from "fabric";
+import React from "react";
 
-export default function ImageCanvas(props: { template: Object }) {
-    console.log(props.template.width)
-    const id_current = React.useRef(null)
-    React.useEffect(() => {
-        const canvas = new fabric.Canvas(id_current.current)
-        fabric.Image.fromURL(`/${props.template.imageUrl}`, function (oImg) {
-            canvas.add(oImg);
-        })
-        return () => {
-            if (canvas)
-                canvas.remove()
-        }
-    }, [id_current.current, props.template.width, props.template.height])
-    return (
-        <div>
-            <canvas ref={id_current} width={props.template.width} height={props.template.height}></canvas>
-        </div>
-    )
+export default function ImageCanvas(props: {
+  template: {
+    imageUrl: string;
+    width: number;
+    height: number;
+  };
+}) {
+  const id_current = React.useRef(null);
+  React.useEffect(() => {
+    if (id_current.current === null) return;
+    if (props.template.width === undefined || props.template.height === undefined) return;
+    if (props.template.imageUrl === undefined) return;
+    const canvas = new fabric.Canvas(id_current.current);
+    fabric.Image.fromURL(`/${props.template.imageUrl}`, function (oImg) {
+      const { width, height } = oImg.getOriginalSize();
+      oImg.scaleX = (props.template.width * 3) / width;
+      oImg.scaleY = (props.template.height * 3) / height;
+      canvas.add(oImg);
+    });
+    canvas.renderAll();
+    return () => {
+      if (canvas) canvas.dispose();
+    };
+  }, [id_current.current, props.template.width, props.template.height]);
+  return (
+    <canvas
+      ref={id_current}
+      width={props.template.width * 3}
+      height={props.template.height * 3}
+      style={{ border: "1px solid black" }}
+    ></canvas>
+  );
 }
--- src/components/Template/editTemplate.tsx
+++ src/components/Template/editTemplate.tsx
@@ -1,110 +1,106 @@
 "use client";
-import TextFieldsIcon from '@mui/icons-material/TextFields';
-import InsertPhotoIcon from '@mui/icons-material/InsertPhoto';
-import Button from '@mui/material/Button';
-import ImageIcon from '@mui/icons-material/Image';
-import FilterIcon from '@mui/icons-material/Filter';
-import { styled } from '@mui/material/styles';
-import React from 'react';
-import ImageCanvas from '../Form/image';
+import TextFieldsIcon from "@mui/icons-material/TextFields";
+import InsertPhotoIcon from "@mui/icons-material/InsertPhoto";
+import Button from "@mui/material/Button";
+import ImageIcon from "@mui/icons-material/Image";
+import FilterIcon from "@mui/icons-material/Filter";
+import { styled } from "@mui/material/styles";
+import React from "react";
+import ImageCanvas from "../Form/image";
 
-const VisuallyHiddenInput = styled('input')({
-    clip: 'rect(0 0 0 0)',
-    clipPath: 'inset(50%)',
-    height: 1,
-    overflow: 'hidden',
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    whiteSpace: 'nowrap',
-    width: 1,
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
 });
 
 export default function EditTemplate(props: { id: string }) {
-    const [template, setTemplate] = React.useState({})
+  const [template, setTemplate] = React.useState({});
 
-    React.useEffect(() => {
-        fetch(`http://192.168.1.222:3000/template/${props.id}`, {
-            method: 'GET'
-        })
-            .then(res => res.json())
-            .then(template => {
-                setTemplate(template)
-            })
-    }, [props.id])
+  React.useEffect(() => {
+    fetch(`http://192.168.1.222:3000/template/${props.id}`, {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((template) => {
+        setTemplate(template);
+      });
+  }, [props.id]);
 
-    console.log(template)
+  console.log(template);
 
-    return (
-        <div>
-            <div className="flex flex-col h-full">
-                <div className="header">
-
-                </div>
-                <div className="main-content">
-                    <div className="layer-content">
-                        <div className="flex px-2 py-4">
-                            <span className="">Layers</span>
-                        </div>
-                        <div className='flex px-5 py-4'>
-                            <span className='w-30'>1</span>
-                            <span>Image</span>
-                        </div>
-                    </div>
-                    <div className="template-content">
-                        {/* <div className='img'>
+  return (
+    <div>
+      <div className="flex flex-col h-full">
+        <div className="header"></div>
+        <div className="main-content">
+          <div className="layer-content">
+            <div className="flex px-2 py-4">
+              <span className="">Layers</span>
+            </div>
+            <div className="flex px-5 py-4">
+              <span className="w-30">1</span>
+              <span>Image</span>
+            </div>
+          </div>
+          <div className="template-content">
+            {/* <div className='img'>
                             <img src={`/${template.imageUrl}`} alt='Image template' width={template.width} height={template.height}></img>
                         </div> */}
-                        <ImageCanvas template={template}></ImageCanvas>
+            <ImageCanvas template={template}></ImageCanvas>
+          </div>
+          <div className="sidebar-content position-relative">
+            <div className="flex">
+              <div className="px-3 py-3">
+                <div className="border w-20 h-20 rounded-lg flex justify-center items-center hover:bg-layer cursor-pointer">
+                  <div className="text-center text-sm">
+                    <div>
+                      <FilterIcon></FilterIcon>
                     </div>
-                    <div className="sidebar-content position-relative">
-                        <div className='flex'>
-                            <div className="px-3 py-3">
-                                <div className="border w-20 h-20 rounded-lg flex justify-center items-center hover:bg-layer cursor-pointer">
-                                    <div className="text-center text-sm">
-                                        <div>
-                                            <FilterIcon></FilterIcon>
-                                        </div>
-                                        Clip art
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="px-3 py-3">
-                                <div className="border w-20 h-20 rounded-lg flex justify-center items-center hover:bg-layer cursor-pointer">
-                                    <div className="text-center text-sm">
-                                        <div>
-                                            <InsertPhotoIcon></InsertPhotoIcon>
-                                        </div>
-                                        Image
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='py-4'>
-                            <div className='px-3'>
-                                <div className='collapse-title min-h-0 bg-layer text-white p-3'>
-                                    Config
-                                </div>
-                                <div className='py-3'>
-                                    <Button
-                                        component="label"
-                                        role={undefined}
-                                        variant="contained"
-                                        tabIndex={-1}
-                                        startIcon={<ImageIcon />}
-                                    >
-                                        Upload image
-                                        <VisuallyHiddenInput type="file" />
-                                    </Button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='px-3 position-absolute'>
-                            <button className='button'>Save</button>
-                        </div>
-                    </div>
+                    Clip art
+                  </div>
                 </div>
+              </div>
+              <div className="px-3 py-3">
+                <div className="border w-20 h-20 rounded-lg flex justify-center items-center hover:bg-layer cursor-pointer">
+                  <div className="text-center text-sm">
+                    <div>
+                      <InsertPhotoIcon></InsertPhotoIcon>
+                    </div>
+                    Image
+                  </div>
+                </div>
+              </div>
             </div>
+            <div className="py-4">
+              <div className="px-3">
+                <div className="collapse-title min-h-0 bg-layer text-white p-3">Config</div>
+                <div className="py-3">
+                  <Button
+                    component="label"
+                    role={undefined}
+                    variant="contained"
+                    tabIndex={-1}
+                    startIcon={<ImageIcon />}
+                  >
+                    Upload image
+                    <VisuallyHiddenInput type="file" />
+                  </Button>
+                </div>
+              </div>
+            </div>
+            <div className="px-3 position-absolute">
+              <button className="button">Save</button>
+            </div>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
