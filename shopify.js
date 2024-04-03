27a29296f4eadc5f0c39bcb42ca211da0fde5d00
ThangNguyen <vietthang.it@ecombest.co>
2024-04-03 09:47:43 +0700

    feat: update shopify js

--- shopify.js
+++ shopify.js
@@ -20,10 +20,57 @@ const getTemplateById = async (id) => {
   return response.json();
 };
 
+const getLayer = async (templateId) => {
+  const response = await fetch(`http://192.168.1.222:3000/layer/template/${templateId}`).then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch layer");
+    }
+    return response;
+  });
+  return response.json();
+}
+
+const getOption = async (optionSetId) => {
+  const response = await fetch(`http://192.168.1.222:3000/option/set/${optionSetId}`).then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch option");
+    }
+    return response;
+  });
+  return response.json();
+}
+
+const getFunc = async (options) => {
+  const response = await fetch(`http://192.168.1.222:3000/function/option-ids`,{
+    method: "POST",
+    headers: {
+      "content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      optionIds: options.map((option) => option.id),
+    }),
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("Failed to fetch option");
+    }
+    return response;
+  });
+  return response.json();
+}
+
 const main = async () => {
   const campaign = await fetchCampaign(8);
   const template = await getTemplateById(campaign.templateId);
-  return template;
+  const layer = await getLayer(campaign.templateId);
+  const options = await getOption(campaign.optionSetId)
+  const func = await getFunc(options)
+  return {
+    campaign: campaign,
+    template: template,
+    layer: layer,
+    options: options,
+    func: func
+  };
 };
 
 (async () => {
@@ -33,9 +80,16 @@ const main = async () => {
 
   canvasTag = document.createElement("canvas");
   canvasTag.id = "canvas";
+  optionTag = document.createElement("div")
+  optionTag.id = "option"
 
-  template = await main();
-  console.log(template);
+  objectRes = await main();
+  template = objectRes.template
+  layers = objectRes.layer
+  options = objectRes.options
+  funcs = objectRes.func
+  campaign = objectRes.campaign
+  // console.log(campaign);
   if (template.width > template.height) {
     if (template.width > MAX_DIMENSION) {
       width = MAX_DIMENSION;
@@ -83,11 +137,30 @@ const main = async () => {
     canvasImg.sendToBack(oImg);
   });
 
-  canvasImg?.renderAll();
+  // canvasImg?.renderAll();
   const canvasContainer = document.getElementsByClassName("canvas-container")[0];
 
   canvasContainer.style.transformOrigin = "0 0";
   canvasContainer.transform = `scale(0.5)`;
   canvasContainer.width = template.width;
   canvasContainer.height = template.height;
+  
+  layers.forEach((currentLayer) => {
+    fabric.Image.fromURL(template.imageUrl, function (oImg) {
+      const { width, height } = oImg.getOriginalSize();
+      oImg.scaleX = currentLayer.width / width;
+      oImg.scaleY = currentLayer.height / height;
+      oImg.top = currentLayer.top;
+      oImg.left = currentLayer.left;
+      oImg.selectable = false;
+      canvasImg.add(oImg);
+    });
+  });
+  optionTag.style = {
+    width: "600px",
+    height: "200px",
+    border: "1px solid black"
+  }
+  container.appendChild(optionTag)
+  canvasImg?.renderAll();
 })();
